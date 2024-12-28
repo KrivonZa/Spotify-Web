b7b7cc666f91ceb2bb3219dc779e2d7670ecc63b
Kevin Truong <kevintruong6587@gmail.com>
2024-12-28 08:05:36 +0700

    Update Language and Player

--- src/layouts/UserLayout/_footer/Footer.tsx
+++ src/layouts/UserLayout/_footer/Footer.tsx
@@ -4,106 +4,117 @@ import {
   TwitterOutlined,
   FacebookOutlined,
 } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <footer className="text-white p-4">
       <div className="container mx-auto">
         <Row gutter={16}>
           <Col xs={24} sm={12} md={6} lg={6} xl={6} className="text-base">
-            <h4 className="font-bold mb-2">Công ty</h4>
+            <h4 className="font-bold mb-2">{t("footer.company")}</h4>
             <ul className="space-y-1 text-[#a0a0a0]">
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Giới thiệu
+                  {t("footer.about")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Việc làm
+                  {t("footer.job")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  For the Record
+                  {t("footer.record")}
                 </button>
               </li>
             </ul>
           </Col>
           <Col xs={24} sm={12} md={6} lg={6} xl={6} className="text-base">
-            <h4 className="font-bold mb-2">Cộng đồng</h4>
+            <h4 className="font-bold mb-2">{t("footer.communities")}</h4>
             <ul className="space-y-1 text-[#a0a0a0]">
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Dành cho các Nghệ sĩ
+                  {t("footer.artist")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Nhà phát triển
+                  {t("footer.developers")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Quảng cáo
+                  {t("footer.ads")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Nhà đầu tư
+                  {t("footer.investor")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Nhà cung cấp
+                  {t("footer.vendor")}
                 </button>
               </li>
             </ul>
           </Col>
           <Col xs={24} sm={12} md={6} lg={6} xl={6} className="text-base">
-            <h4 className="font-bold mb-2">Liên kết hữu ích</h4>
+            <h4 className="font-bold mb-2">{t("footer.link")}</h4>
             <ul className="space-y-1 text-[#a0a0a0]">
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Hỗ trợ
+                  {t("footer.support")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Ứng dụng Di động Miễn phí
+                  {t("footer.app")}
                 </button>
               </li>
             </ul>
           </Col>
           <Col xs={24} sm={12} md={6} lg={6} xl={6} className="text-base">
-            <h4 className="font-bold mb-2">Các gói của Spotify</h4>
+            <h4 className="font-bold mb-2">{t("footer.plans")}</h4>
             <ul className="space-y-1 text-[#a0a0a0]">
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Premium Individual
+                  {t("footer.individual")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Premium Student
+                  {t("footer.student")}
                 </button>
               </li>
               <li>
                 <button className="hover:text-white hover:underline duration-150">
-                  Spotify Free
+                  {t("footer.free")}
                 </button>
               </li>
             </ul>
           </Col>
         </Row>
         <div className="flex justify-center mt-10">
-          <a href="#" className="mr-4 hover:bg-[#414141] duration-200 rounded-full px-3 py-3">
+          <a
+            href="#"
+            className="mr-4 hover:bg-[#414141] duration-200 rounded-full px-3 py-3"
+          >
             <InstagramOutlined style={{ fontSize: 24 }} />
           </a>
-          <a href="#" className="mr-4 hover:bg-[#414141] duration-200 rounded-full px-3 py-3">
+          <a
+            href="#"
+            className="mr-4 hover:bg-[#414141] duration-200 rounded-full px-3 py-3"
+          >
             <TwitterOutlined style={{ fontSize: 24 }} />
           </a>
-          <a href="#" className="mr-4 hover:bg-[#414141] duration-200 rounded-full px-3 py-3">
+          <a
+            href="#"
+            className="mr-4 hover:bg-[#414141] duration-200 rounded-full px-3 py-3"
+          >
             <FacebookOutlined style={{ fontSize: 24 }} />
           </a>
         </div>
